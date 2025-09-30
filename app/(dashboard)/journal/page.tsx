ac235f3a196d30b612de94e081056dac4d55b8d8
Onur Ucar <hello@ucaronur.com>
2025-09-29 19:56:21 -0500

    [fix] Fix inconsistent db provider

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,38 +1,44 @@
 import EntryCard from '@/components/EntryCard';
-import NewEntryCard from '@/components/NewEntryCard';
-import { getUserByClerkID } from '@/utils/auth';
+import NewEntry from '@/components/NewEntry';
+import Question from '@/components/Question';
+import { qa } from '@/utils/ai';
+import { getUserFromClerkID } from '@/util/auth';
 import { prisma } from '@/utils/db';
 import Link from 'next/link';
-import { analyze } from '@/utils/ai';
 
 const getEntries = async () => {
-  const user = await getUserByClerkID();
-  const entries = await prisma.journalEntry.findMany({
+  const user = await getUserFromClerkID();
+  const data = await prisma.journalEntry.findMany({
     where: {
       userId: user.id,
     },
     orderBy: {
       createdAt: 'desc',
     },
+    include: {
+      analysis: true,
+    },
   });
 
-  await analyze('hello, how are you cuh!');
-
-  return entries;
+  return data;
 };
 
 const JournalPage = async () => {
-  const entries = await getEntries();
-
+  const data = await getEntries();
   return (
-    <div className='p-10 bg-zinc-400/10 h-full'>
-      <h2 className='text-3xl mb-8'>Journal</h2>
-      <div className='grid grid-cols-3 gap-4 '>
-        <NewEntryCard />
-        {entries.map((entry) => (
-          <Link href={`/journal/${entry.id}`} key={entry.id}>
-            <EntryCard entry={entry} />
-          </Link>
+    <div className='px-6 py-8 bg-zinc-100/50 h-full'>
+      <h1 className='text-4xl mb-12'>Journals</h1>
+      <div className='my-8'>
+        <Question />
+      </div>
+      <div className='grid grid-cols-3 gap-4'>
+        <NewEntry />
+        {data.map((entry) => (
+          <div key={entry.id}>
+            <Link href={`/journal/${entry.id}`}>
+              <EntryCard entry={entry} />
+            </Link>
+          </div>
         ))}
       </div>
     </div>
--- utils/ai.ts
+++ utils/ai.ts
@@ -1,24 +1,29 @@
-import { OpenAI } from 'langchain';
-import { StructuredOutputParser } from 'langchain/output_parsers';
+import { OpenAI } from '@langchain/openai';
+import { StructuredOutputParser } from '@langchain/core/output_parsers';
+import { PromptTemplate } from '@langchain/core/prompts';
 import z from 'zod';
-import { PromptTemplate } from 'langchain/prompts';
 
 const parser = StructuredOutputParser.fromZodSchema(
   z.object({
     mood: z
       .string()
       .describe('the mood of the person who wrote the journal entry.'),
-    summary: z.string().describe('quick summary of the entire entry.'),
     subject: z.string().describe('the subject of the journal entry.'),
     negative: z
       .boolean()
       .describe(
         'is the journal entry negative? (i.e. does it contain negative emotions?).'
       ),
+    summary: z.string().describe('quick summary of the entire entry.'),
     color: z
       .string()
       .describe(
         'a hexidecimal color code that represents the mood of the entry. Example #0101fe for blue representing happiness.'
+      ),
+    sentimentScore: z
+      .number()
+      .describe(
+        'sentiment of the text and rated on a scale from -10 to 10, where -10 is extremely negative, 0 is neutral, and 10 is extremely positive.'
       ),
   })
 );
@@ -28,7 +33,7 @@ const getPrompt = async (content) => {
 
   const prompt = new PromptTemplate({
     template:
-      'Analyze the following journal entry. Follow the instructions and format your response to match the format instructions, no matter what! \n{format_instructions}\n{entry}',
+      'Analyze the following journal entry. Follow the intrusctions and format your response to match the format instructions, no matter what! \n{format_instructions}\n{entry}',
     inputVariables: ['entry'],
     partialVariables: { format_instructions },
   });
@@ -40,14 +45,40 @@ const getPrompt = async (content) => {
   return input;
 };
 
-export const analyze = async (content) => {
-  const input = await getPrompt(content);
+export const analyzeEntry = async (entry) => {
+  const input = await getPrompt(entry.content);
   const model = new OpenAI({ temperature: 0, modelName: 'gpt-3.5-turbo' });
-  const result = await model.call(input);
+  const output = await model.call(input);
 
   try {
-    return parser.parse(result);
+    return parser.parse(output);
   } catch (e) {
-    console.log(e);
+    const fixParser = OutputFixingParser.fromLLM(
+      new OpenAI({ temperature: 0, modelName: 'gpt-3.5-turbo' }),
+      parser
+    );
+    const fix = await fixParser.parse(output);
+    return fix;
   }
+};
+
+export const qa = async (question, entries) => {
+  const docs = entries.map(
+    (entry) =>
+      new Document({
+        pageContent: entry.content,
+        metadata: { source: entry.id, date: entry.createdAt },
+      })
+  );
+  const model = new OpenAI({ temperature: 0, modelName: 'gpt-3.5-turbo' });
+  const chain = loadQARefineChain(model);
+  const embeddings = new OpenAIEmbeddings();
+  const store = await MemoryVectorStore.fromDocuments(docs, embeddings);
+  const relevantDocs = await store.similaritySearch(question);
+  const res = await chain.call({
+    input_documents: relevantDocs,
+    question,
+  });
+
+  return res.output_text;
 };
